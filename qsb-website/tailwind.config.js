9e7b3720569dd006538eaee991e6f501ecddf852
Olga <omfe1811@gmail.com>
2025-07-08 16:00:20 +0200

    fuckery

--- qsb-website/tailwind.config.js
+++ qsb-website/tailwind.config.js
@@ -1,8 +1,30 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+	mode: "jit",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				darkPurple: "var(--color-darkPurple, #32146a)",
+				darkestPurple: "var(--color-darkestPurple, #180a34ff)",
+				vibrantOrange: "var(--color-vibrantOrange, #f75c03)",
+				teaGreen: "var(--color-teaGreen, #d0e3c4)",
+				magenta: "var(--color-magenta, #c45ab3)",
+				lightPink: "var(--color-lightPink, #ffe5ea)",
+			},
+			fontFamily: {
+				hagridItalic: ['"Hagrid Text Extrabold Italic"', "sans-serif"],
+				hagrid: ['"Hagrid Text Extrabold"', "sans-serif"],
+				hagridRegular: ['"Hagrid Regular"', "sans-serif"],
+				hagridAltItalic: ['"Hagrid Italic"', "sans-serif"],
+				alba: ['"Alba Regular"', "sans-serif"],
+				albaSuper: ['"Alba Super"', "sans-serif"],
+				albaModern: ['"Alba Modern"', "sans-serif"],
+				royalacid: ['"Royalacid"', "cursive"],
+				royalacidOutline: ['"Royalacid Outline"', "cursive"],
+				allstar: ['"All Star Resort"', "cursive"],
+				neon: ['"Neon"', "cursive"],
+			},
+		},
 	},
 	plugins: [],
 };
--- qsb-website/vite.config.js
+++ qsb-website/vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
 	plugins: [react()],
+	css: {
+		postcss: "./postcss.config.js", // ensure Vite knows where your PostCSS config is
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				main: path.resolve(__dirname, "index.html"),
+				design: path.resolve(__dirname, "design.html"),
+			},
+		},
+	},
 });
